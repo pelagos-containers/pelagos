e65315d4a9ca1b364cdbee791b34efdd1d759e41
Ubuntu <ubuntu@ip-172-31-13-76.us-west-2.compute.internal>
2022-07-10 18:43:56 +0000

    fix launch script for sudo and log

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ALPINE_BUILD=example-20220710.tar.gz
 cp ../alpine-make-rootfs/$ALPINE_BUILD ./ && sudo rm -fr ./alpine-rootfs && mkdir ./alpine-rootfs && cd ./alpine-rootfs/ && tar zxvf ../$ALPINE_BUILD ./ && cd ..
-# export RUST_LOG=info
-# export RUST_BACKTRACE=full
-sudo ./target/debug/remora --exe /bin/ash --rootfs ./alpine-rootfs
+export RUST_LOG=info
+export RUST_BACKTRACE=full
+sudo -E ./target/debug/remora --exe /bin/ash --rootfs ./alpine-rootfs
